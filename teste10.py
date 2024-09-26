934c42e7b4b16b785f02e8d21a97dad137ef00ca
Lino <linolegal3@gmail.com>
2024-09-26 18:31:33 -0300

    andre

--- teste10.py
+++ teste10.py
@@ -1,14 +1,14 @@
-"""
-for numero in range(1, 51):
-    print(numero)
 
-#for x in range (1934,2027,4):
-#    print(f'copa do mundo-{x}')
-"""
-import random
+# for numero in range(1, 51):
+#     print(numero)
 
-for x in range(1,2):
-    print(f'\nnumeros da sorte{x}')
-    for y in range(15):
-        num = random.randint(1,80)
-        print(f':{num}')
+# for x in range (1930,2027,4):
+#     print(f'copa do mundo-{x}')
+
+# import random
+
+# for x in range(1,2):
+#     print(f'\nnumeros da sorte{x}')
+#     for y in range(5):
+#         num = random.randint(1,80)
+#         print(f':{num}')
